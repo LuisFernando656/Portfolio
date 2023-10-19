939385a229235c9e7fc3d94a559f0638eaa790cd
Luis Fernando <luisfernandobega@gmail.com>
2023-10-19 12:26:48 -0300

    first modifications and deploy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,30 @@ import './styles.css'
 
 export const Home = () => {
   return (
-    <h1>ola</h1>
+    <div className='home'>
+      <main>
+        <header>
+          <div className='info'>
+            <img src="https://github.com/luisfernando656.png" alt="Foto de perfil" />
+            
+            <div>
+              <h1>Luis Fernando de Oliveira</h1>
+              <h2>Web Developer</h2>
+            </div>
+          </div>
+          <nav>
+            <ul className='nav-list'>
+              <li><a href="/">Home</a></li>
+              <li><a href="/">Projects</a></li>
+              <li><a href="/">About</a></li>
+              <li><a href="/">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+
+        </main>
+      </main>
+    </div>
   )
 }
